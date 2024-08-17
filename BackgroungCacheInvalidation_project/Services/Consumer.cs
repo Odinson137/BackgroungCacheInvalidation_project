612cc53638fd10d82970363f4e31a75e5fdc8689
Odinson137 <yuriy_b@holdingbp.ru>
2024-08-17 12:38:19 +0300

    add test project

---
+++ BackgroungCacheInvalidation_project/Services/Consumer.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+
+namespace BackgroungCacheInvalidation_project.Services;
+
+public class Consumer : BackgroundService
+{
+    private readonly ISubscriber _subscriber;
+    private readonly ILogger<Consumer> _logger;
+    private const string Channel = "messages";
+    
+    public Consumer(IConnectionMultiplexer connectionMultiplexer, ILogger<Consumer> logger)
+    {
+        _subscriber = connectionMultiplexer.GetSubscriber();
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await _subscriber.SubscribeAsync(Channel, (channel, message) =>
+        {
+            _logger.LogInformation($"Hello from consumer with message! {channel}: {message}");
+        });
+    }
+}
---
+++ BackgroungCacheInvalidation_project/Services/Producer.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+
+namespace BackgroungCacheInvalidation_project.Services;
+
+public class Producer : BackgroundService
+{
+    private readonly ISubscriber _subscriber;
+    private readonly ILogger<Consumer> _logger;
+    private const string Channel = "messages";
+    
+    public Producer(IConnectionMultiplexer connectionMultiplexer, ILogger<Consumer> logger)
+    {
+        _subscriber = connectionMultiplexer.GetSubscriber();
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _subscriber.PublishAsync(Channel, "Produce!");
+
+            _logger.LogInformation(
+                $"Sending message to {Channel}");
+
+            await Task.Delay(5000, stoppingToken);
+        }
+    }
+}
